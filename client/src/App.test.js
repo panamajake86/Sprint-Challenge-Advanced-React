d880cdb410fc1b7e7005a25821e690ac97989bac
Jake <panamajake86@gmail.com>
2019-11-08 22:10:19 -0700

    Well, here's my last commit.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,31 +1,35 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 import DarkMode from './Components/DarkMode';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
 
-// const show = require('./App');
+const show = require('./App');
 
-// describe('App', ()=>{
-//   describe('did component mount', ()=>{
-//     it('component mounted', ()=>{
-//       const expectedOutput = state !== 0;
-//       const actualOutput = show.state;
-//       expect(actualOutput).toBe(expectedOutput);
-//     });
-//   });
-// });
+describe('App', ()=>{
+  describe('did component mount', ()=>{
+    it('component mounted', ()=>{
+      const expectedOutput = state !== 0;
+      const actualOutput = show.state;
+      expect(actualOutput).toBe(expectedOutput);
+    });
+  });
+});
 
 test('render without crashing', () => {
   render(<DarkMode />);
 });
 
-// test('it returns "hello world"', () => {
-//   expect(helloWorld()).toBe("hello world");
-//   expect(helloWorld()).not.toBe("");
-// });
+test('render without crashing', () => {
+  render(<App />);
+});
+
+test('it returns "hello world"', () => {
+  expect(helloWorld()).toBe("hello world");
+  expect(helloWorld()).not.toBe("");
+});
