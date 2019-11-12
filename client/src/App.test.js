39c4f01a225c91bff46ca57a4a53565f456ac815
Jake <panamajake86@gmail.com>
2019-11-11 20:43:46 -0700

    Got my tests working, this should be good to go.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import DarkMode from './Components/DarkMode';
+import useDarkMode from './Hooks/useDarkMode';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -9,29 +10,14 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-
-const show = require('./App');
-
-describe('App', ()=>{
-  describe('did component mount', ()=>{
-    it('component mounted', ()=>{
-      const expectedOutput = state !== 0;
-      const actualOutput = show.state;
-      expect(actualOutput).toBe(expectedOutput);
-    });
-  });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<DarkMode />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
 
-test('render without crashing', () => {
-  render(<DarkMode />);
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<useDarkMode />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
-
-test('render without crashing', () => {
-  render(<App />);
-});
-
-test('it returns "hello world"', () => {
-  expect(helloWorld()).toBe("hello world");
-  expect(helloWorld()).not.toBe("");
-});
-
